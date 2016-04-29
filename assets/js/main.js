6e8c0ea788abcd359bafbdfe956c8fded8219fb6
Sirius <sirius@sirius-S550CB.(none)>
2016-04-30 01:01:30 +0800

    change nav and complete china logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    side_nav();
+    $(document).delegate('.open_nav', 'click', function(event) {
+        $(this).addClass('oppenned');
+        event.stopPropagation();
+    })
+    $(document).delegate('body', 'click', function(event) {
+        $('.open_nav').removeClass('oppenned');
+    })
+
+    $(document).delegate('.cls', 'click', function(event) {
+        $('.open_nav').removeClass('oppenned');
+        event.stopPropagation();
+    });
+    $(document).delegate('.sub-menu', 'click', function(event) {
+        $('.open_nav').removeClass('oppenned');
+        event.stopPropagation();
+    });
+
     parallax_scroll();
     ractive_init_tw(function() {
         modal();
@@ -7,14 +23,17 @@ $(document).ready(function() {
     ractive_init_cn(function() {
         modal();
     });
+    Ractive.DEBUG = false;
+    // side_nav();
     // $('.scrollspy').scrollSpy();
+
 });
 
 function ractive_init_tw(callback) {
     $.get("assets/companys.xml", function(xml) {
         var obj_companys = $.xml2json(xml);
         var stringified = JSON.stringify(obj_companys);
-        stringified = stringified.replace(/[\r\n\t]/g,"<br/>");
+        stringified = stringified.replace(/[\r\n\t]/g, "<br/>");
         var companys = JSON.parse(stringified);
         var ractive = new Ractive({
             el: '.companys',
@@ -31,7 +50,7 @@ function ractive_init_cn(callback) {
     $.get("assets/china.xml", function(xml) {
         var obj_companys = $.xml2json(xml);
         var stringified = JSON.stringify(obj_companys);
-        stringified = stringified.replace(/[\r\n\t]/g,"<br/>");
+        stringified = stringified.replace(/[\r\n\t]/g, "<br/>");
         var china = JSON.parse(stringified);
         var ractive = new Ractive({
             el: '.china',
@@ -48,7 +67,7 @@ function modal() {
     $('.modal-trigger').leanModal();
 };
 
-function side_nav(){
+function side_nav() {
     $('.button-collapse').sideNav({
         menuWidth: 120,
         edge: 'left',
@@ -56,7 +75,9 @@ function side_nav(){
 };
 
 // parallax scrolling
-function parallax_scroll(){
+function parallax_scroll() {
+    $('#top').parallax("50%", 0.2);
+    $('#job').parallax("50%", 0.1);
     // scroll smoothly
     $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -64,12 +85,11 @@ function parallax_scroll(){
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
-                    scrollTop: target.offset().top - 50
+                    scrollTop: target.offset().top
                 }, 1000);
                 return false;
             }
         }
     });
-    $('#top').parallax("50%", 0.2);
-    $('#job').parallax("50%", 0.1);
+    
 }
